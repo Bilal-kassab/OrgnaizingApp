d3432ff55f9eb30138ee4e98db7ec028065da5a5
Bilal-kassab <bilok8218@gmail.com>
2025-04-14 14:28:34 +0300

    9th commit

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,7 +12,7 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable=[
-        'description','name','room_id','user_id','money'
+        'description','name','room_id','user_id','money',
     ];
 
     public function room():BelongsTo
